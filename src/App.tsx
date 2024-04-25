f9d9430a88e1f247f0e014bf661f4f656f182e21
varvaratikh <tikhonovavarvara181203@gmail.com>
2024-04-25 19:05:06 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/main_page/header/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Chat from './pages/Chat';
+import HomePage from "./components/main_page/middle/HomePage";
+import NewsHomePage from "./components/main_page/news_fragment/NewsHomePage";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage'
 
 const App: React.FC = () => {
     return (
         <Router>
+            <div>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/chat" element={<Chat />} />
+                </Routes>
+                <HomePage/>
+                <NewsHomePage/>
+            </div>
+        </Router>
+    );
+};
             <Routes>
                 <Route path="/" element={<LoginPage />} />
             </Routes>
