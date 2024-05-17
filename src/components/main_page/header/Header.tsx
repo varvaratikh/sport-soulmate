9473d98766e69cf26d227fd3cdb2f2af850e2621
varvaratikh <tikhonovavarvara181203@gmail.com>
2024-05-18 00:21:01 +0300

    change header on main page

--- src/components/main_page/header/Header.tsx
+++ src/components/main_page/header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './header.sass';
+import '../../../styles/header.sass';
 // @ts-ignore
 import logo from '../../../assets/logo-header.png';
 
