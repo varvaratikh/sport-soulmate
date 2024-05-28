c55f1898196572bd617d3448e920721a8ba8dca7
Аида Вафина <aidavafina@MacBook-Pro-Aida.local>
2024-05-28 18:01:09 +0300

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthProvider } from './contexts/AuthContext'; // Импортируйте AuthProvider
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <AuthProvider> {/* Оберните App в AuthProvider */}
+            <App />
+        </AuthProvider>
+    </React.StrictMode>
 );
